9e220eea7607f7140a87a28abea5e27762a1da34
Alumno3 <villamario7@digitechfp.com>
2025-09-30 16:48:59 +0200

    revision

--- src/app/pages/extra/extra.page.ts
+++ src/app/pages/extra/extra.page.ts
@@ -14,13 +14,14 @@ import {
 } from '@ionic/angular/standalone';
 import {HeaderComponent} from "../../components/header/header.component";
 import {RouterLink} from "@angular/router";
+import {MenuComponent} from "../../components/menu/menu.component";
 
 @Component({
   selector: 'app-extra',
   templateUrl: './extra.page.html',
   styleUrls: ['./extra.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, IonCol, IonGrid, IonButtons, IonRow, IonButton, RouterLink, IonMenuToggle, IonText]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, IonCol, IonGrid, IonButtons, IonRow, IonButton, RouterLink, IonMenuToggle, IonText, MenuComponent]
 })
 export class ExtraPage implements OnInit {
 
